2cc64dce34db31e0960561a7343f81b89a4e4b99
fcamachos <fcamachos@ciencias.unam.mx>
2020-12-08 11:57:42 -0600

    Fixed minor bugs. Icons now work even if you don't extact the Folder in your home directory

--- Kupdate.sh
+++ Kupdate.sh
@@ -3,36 +3,42 @@
 ## Asks to the user for his SU password! 
 ## This script must be used at its own caution.
 
+## Folder where script is stored. For more info visit: https://stackoverflow.com/a/4774063
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 ## Declare de title in your language 
 title="Kupdate"
 ## Write the SU password
 message="Write your SU password"
 ## The string to be looked if everything is up to date
-updated="^All packages are up-to-date"
+# updated="^All packages are up-to-date"
+
+## Translation for Spanish devices
+updated="^Todos los paquetes están actualizados"
 
 ## Shows a passive popup Looking for updates
 snd_begin(){
-	kdialog --icon ~/Scripts/update.png --title"$title" --passivepopup \
+	kdialog --icon "$SCRIPTPATH"/update.png --title "$title" --passivepopup \
 	"Looking for updates" 2
 }
 ## Shows a passive popup when the update ends with an error
 snd_error(){
-	kdialog --icon ~/Scripts/updateF.png --title "$title" --passivepopup \
+	kdialog --icon "$SCRIPTPATH"/updateF.png --title "$title" --passivepopup \
 	"Updating error" 5
 }
 ## Shows a passive popup when there are no updates aviable
 snd_noUpdate(){
-	kdialog --icon ~/Scripts/updateF.png --title "$title" --passivepopup \
+	kdialog --icon "$SCRIPTPATH"/updateF.png --title "$title" --passivepopup \
 	"System is up to date" 5
 }
 ## Shows a passive popup when the update finish correctly
 snd_complete(){
-	kdialog --icon ~/Scripts/update.png --title "$title" --passivepopup \
+	kdialog --icon "$SCRIPTPATH"/update.png --title "$title" --passivepopup \
 	"System updated" 5
 }
 ## Shows a passive popup when the user cancels the update
 snd_cancel(){
-	kdialog --icon ~/Scripts/updateF.png --title "$title" --passivepopup \
+	kdialog --icon "$SCRIPTPATH"/updateF.png --title "$title" --passivepopup \
 	"Update canceled" 2
 }
 
@@ -45,7 +51,7 @@ if [ $? = 0 ]; then
 	## send the notification of begining
  	snd_begin
 	## run the aux script with SU privileges!
-	echo $PASSWD | sudo -S sh ~/Scripts/update_aux.sh "$updated"
+	echo $PASSWD | sudo -S sh update_aux.sh "$updated"
 	## check the exit code of the aux script
 	case $? in
 		0)
--- update_aux.sh
+++ update_aux.sh
@@ -1,17 +1,20 @@
 #!/bin/bash 
 
 ## Check if the update command has the key word when system is updated
-log={ apt update 2>&1 } | grep -i "$1" ##"$updated"
+log= apt update 2>&1 | grep -i "$1" ##"$updated"
+echo $log
 ## If system is updated do nothing
-if $log ; then    
+if apt update 2>&1 | grep -i "$1" ; then    
     echo "up to date"
-    echo "$1"
     exit 1      #snd_noUpdate
 ## If there are updates aviable run the upgrade
 else
     echo "update needed"
-    sudo -S konsole --hold -e apt upgrade -y
-    if [$? = 0]; then
+    ## if you don't want the konsole to show, just toggle the comments of the next 2 lines
+    # sudo -S apt upgrade -y
+    sudo -S konsole -e apt upgrade -y
+    ####
+    if [$? = 0] ; then
     	exit 0 #snd_complete
     else
     	exit 2 #snd_error
