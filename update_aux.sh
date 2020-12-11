b5b3f4e800f8d071abf7fa142c91d655d36d0219
fcamachos <fcamachos@ciencias.unam.mx>
2020-12-11 17:36:05 -0600

    Bugs fixed. Now the upgrade process is hidden, if you want to see it run the script in a terminal.

--- update_aux.sh
+++ update_aux.sh
@@ -9,23 +9,22 @@
 ## If system is updated do nothing
 if ! apt update 2>&1 | grep -i "upgradable" ; then    
     echo "exit code $?"
-    echo "up to date"
-    
+    echo "up to date"    
     exit 3      #snd_noUpdate
 ## If there are updates aviable run the upgrade
 else
     echo "update needed"
-    ## if you don't want the konsole to show, just toggle the comments of the next 2 lines
-    # sudo -S apt upgrade -y
-    sudo -S konsole --hold -e apt upgrade -y
-    ####
-    if [$? = 0] ; then
+    ## send to konsole and to the logfile the upgrade process. 
+    ## If you don't want to overwrite it add -a to the parameter
+    ## Example: apt upgrade -y 2>&1 | tee -a /tmp/logfile.log
+    apt upgrade -y 2>&1 | tee /tmp/KupdateLogfile.log
+    echo "exit code $?"
+    if grep "^[0].*[0].*[0].*[0]" /tmp/KupdateLogfile.log ; then         
+        echo "upgrade complete"
+        exit 0 #snd_complete
+    else
         echo "exit code $?"
-        echo "update complete"        
-    	exit 0 #snd_complete
-    else        
-        echo "exit code $?"
-        echo "unable to upgrade"        
+        echo "unable to upgrade, see /tmp/KupdateLogfile.log for details"
         exit 2 #snd_error     	
     fi
 fi	
